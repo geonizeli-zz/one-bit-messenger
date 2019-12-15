b75ce1ba9c37618edfeb63b92e3d7a5666a5ee2c
João <jvsouzax@gmail.com>
2019-12-15 10:36:28 -0300

    <add> profile photo to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :contacts
   has_many :sent, class_name: 'Message', foreign_key: 'sender_id'
   has_many :received, class_name: 'Message', foreign_key: 'receiver_id'
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
