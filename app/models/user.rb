34dd2c1f6808fe020d7aeb48b0b6bf3fe4390b42
João <jvsouzax@gmail.com>
2019-12-15 09:54:14 -0300

    <add> Message and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :contacts
+  has_many :sent, class_name: 'Message', foreign_key: 'sender_id'
+  has_many :received, class_name: 'Message', foreign_key: 'receiver_id'
+
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 end
