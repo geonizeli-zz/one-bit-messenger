f6c4473e4b2582d9fb1de0863de070a886565a50
João <jvsouzax@gmail.com>
2019-12-15 19:23:35 -0300

    <change> sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_12_15_214857) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,8 +37,8 @@ ActiveRecord::Schema.define(version: 2019_12_15_214857) do
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "record_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "record_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["record_id"], name: "index_contacts_on_record_id"
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 2019_12_15_214857) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
-    t.integer "sender_id", null: false
-    t.integer "receiver_id", null: false
+    t.bigint "sender_id", null: false
+    t.bigint "receiver_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["receiver_id"], name: "index_messages_on_receiver_id"
